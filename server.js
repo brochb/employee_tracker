aa3f2fc0f970a609bb71f6aa8b0215fdc9f4dbf0
Brochb <brochbaltzer@gmail.com>
2023-09-23 15:30:13 -0600

    added inquirer and other nessecary npm packages to dependencies, built base inquirer function

--- server.js
+++ server.js
@@ -1,15 +1,87 @@
-const express = require('express').Router();
-const path = require('path');
+const connection = require('./config/connection');
+const inquirer = require('inquirer');
+const figlet = require('figlet');
 const mysql = require('mysql2');
-const fs = require('fs/promises')
 
-const app = express();
+const userPrompt = () => {
+    inquirer.prompt([
+        {
+            name: 'choices',
+            type: 'list',
+            message: 'Please select an option:',
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add Department',
+                'Add Role',
+                'Add Employee',
+                'Update Employee Role',
+                'Remove Employee',
+                'Remove Role',
+                'Remove Department',
+                'Exit'
+            ]
+        }
+    ])
+        .then((answers) => {
+            const { choices } = answers;
 
-const PORT = process.env.PORT || 3001;
+            if (choices === 'View All Employees') {
+                viewAllEmployees();
+            }
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
-app.use(express.json());
+            if (choices === 'View All Departments') {
+                viewAllDepartments();
+            }
+
+            if (choices === 'Add Employee') {
+                addEmployee();
+            }
+
+            if (choices === 'Remove Employee') {
+                removeEmployee();
+            }
+
+            if (choices === 'Update Employee Role') {
+                updateEmployeeRole();
+            }
+
+
+            if (choices === 'View All Roles') {
+                viewAllRoles();
+            }
+
+            if (choices === 'Add Role') {
+                addRole();
+            }
+
+            if (choices === 'Remove Role') {
+                removeRole();
+            }
+
+            if (choices === 'Add Department') {
+                addDepartment();
+            }
+
+            if (choices === 'Remove Department') {
+                removeDepartment();
+            }
+
+            if (choices === 'Exit') {
+                connection.end();
+            }
+        });
+};
+// View all Departments, Roles, Employees
+
+// Add Department, Role, Employee
+
+// Update Employee Roll
+
+// Remove Department, Role, Employee
+
+
 
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
