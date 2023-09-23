a9ddde78dd07593a5becdad47c4ea6b27f9e63cb
Brochb <brochbaltzer@gmail.com>
2023-09-23 15:32:27 -0600

    edited schema to be inline with starterREADME

--- db/schema.sql
+++ db/schema.sql
@@ -24,13 +24,9 @@ CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
+    roles_id INT,
     manager_id INT,
     is_manager BOOLEAN DEFAULT false,
-    departments_id INT,
-    FOREIGN KEY (departments_id)
-    REFERENCES departments(id)
-    ON DELETE SET NULL,
-    roles_id INT,
     FOREIGN KEY (roles_id)
     REFERENCES roles(id)
     ON DELETE SET NULL,
